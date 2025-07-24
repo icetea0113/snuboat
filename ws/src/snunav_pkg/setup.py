d2f7ba8ed7a4477f808d73b4bc328fa0ea8c8e2c
Icetea0113 <v2msyo@naver.com>
2025-07-24 14:48:01 +0900

    temp

--- ws/src/snunav_pkg/setup.py
+++ ws/src/snunav_pkg/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/msg', ['msg/MissionDirector.msg', 'msg/Sensor.msg']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +21,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'motor_interface = snunav_pkg.motor_interface:main',
+            'navigation = snunav_pkg.navigation:main',
         ],
     },
 )
