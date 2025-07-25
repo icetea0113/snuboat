a9fd76e133a68fc5809d9eea933bdc323a6faf11
Icetea0113 <v2msyo@naver.com>
2025-07-25 18:25:05 +0900

    update

--- ws/src/snunav_pkg/snunav_pkg/mission_director 2.py
+++ ws/src/snunav_pkg/snunav_pkg/mission_director 2.py
@@ -16,40 +16,36 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
-from snumsg_pkg.msg import MissionCode
 
-class MissionDirector(Node):
+
+class MinimalPublisher(Node):
 
     def __init__(self):
-        super().__init__('mission_director')
+        super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
-        self.
-
     def timer_callback(self):
-        msg = MissionCode()
-        msg.tick.sec = self.i  # Example usage of tick
-        msg.tick.nanosec = 0  # Set nanoseconds to zero for simplicity
-        msg.value = 1111111111111111  # Example mission code logic
-        # msg.data = 'Hello World: %d' % self.i
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.value)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
+
 
 def main(args=None):
     rclpy.init(args=args)
 
-    mission_director = MissionDirector()
+    minimal_publisher = MinimalPublisher()
 
-    rclpy.spin(mission_director)
+    rclpy.spin(minimal_publisher)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    mission_director.destroy_node()
+    minimal_publisher.destroy_node()
     rclpy.shutdown()
 
 
