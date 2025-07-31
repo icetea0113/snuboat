65113b2b21a764bedb37e2257d24ea6821014af2
InputYourNamePlz <samuel1127@snu.ac.kr>
2025-07-31 13:50:11 +0900

    deleted install, logs folder
    should be re-built in Jetson(arm64)

--- ws/install/snumsg_pkg/local/lib/python3.10/dist-packages/snumsg_pkg/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from snumsg_pkg.msg._mission_code import MissionCode  # noqa: F401
-from snumsg_pkg.msg._sensor import Sensor  # noqa: F401
--- ws/install/snumsg_pkg/share/snumsg_pkg/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(snumsg_pkg_IDL_FILES "msg/MissionCode.idl;msg/Sensor.idl")
-set(snumsg_pkg_INTERFACE_FILES "msg/MissionCode.msg;msg/Sensor.msg")
