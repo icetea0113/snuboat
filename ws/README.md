9986a4b41cc2809254600fa8a52968e101cf8a93
icetea0113 <icetea0113@gmail.com>
2025-08-03 13:32:11 +0900

    SILS 0.0.1

--- ws/README.md
+++ ws/README.md
@@ -1,1 +1,15 @@
 This is ROS 2 Humble Workspace folder.
+
+# 1. 워크스페이스 루트로 이동
+cd ~/snu_boat_ws
+
+# 2. 의존성 업데이트 및 특정 패키지(my_pkg)만 빌드
+rosdep update
+rosdep install --from-paths src --ignore-src -r -y
+colcon build --packages-select snumsg_pkg snunav_pkg --symlink-install
+
+# 3. 빌드 결과 적용 (둘 중 하나만 하여도 됨)
+source ~/snu_boat_ws/install/setup.bash
+source ~/snu_bost_ws/install/local_setup.bash
+
+source ~/.bashrc
---
+++ ws/src/snunav_pkg/launch/sils.launch.py
@@ -0,0 +1,60 @@
+import os
+import yaml
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource, FrontendLaunchDescriptionSource
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    
+    # Config 파일 읽기
+    config_path = os.path.join(
+        get_package_share_directory('snunav_pkg'),
+        'config',
+        'params.yaml'
+    )
+    
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    
+    # 파라미터 추출
+    mission_params = {}
+    controller_params = {}
+    
+    if 'mission_director' in config:
+        mission_params = config.get('mission_director', {}).get('ros__parameters', {})
+    if 'controller' in config:
+        controller_params = config.get('controller', {}).get('ros__parameters', {})
+    
+    # sensor_mode 값 가져오기
+    sensor_mode = mission_params.get('sensor_mode', '0')
+    
+    # 기존 노드들
+    ld.add_action(Node(
+        package='snunav_pkg',
+        executable='mission_director',
+        name='mission_director_node',
+        output='screen',
+        parameters=[mission_params]
+    ))
+    
+    ld.add_action(Node(
+        package='snunav_pkg',
+        executable='controller',
+        name='controller_node',
+        output='screen',
+        parameters=[controller_params]
+    ))
+    
+    ld.add_action(Node(
+        package='snunav_pkg',
+        executable='navigation',
+        name='navigation_node',  # controller_node에서 수정
+        output='screen',
+    ))
+    
+    return ld
